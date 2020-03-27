ea17b7cbbe3c42dfad2a704322a56725514d109f
Adam Garwacki <adam.garwacki@gmail.com>
2020-03-27 14:10:53 +0100

    Stworzenie plików 'index.html' oraz 'main.js'. Stworzenie funkcji dodającej dwie zmienne.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let liczbaPierwsza = 4;
+let liczbaDruga = 3;
+
+function dodawanieLiczb(liczbaPierwsza, liczbaDruga) {
+    // let suma = liczbaPierwsza + liczbaDruga;
+    // return suma;
+    return liczbaPierwsza + liczbaDruga;
+}
+
+console.log(dodawanieLiczb(liczbaPierwsza, liczbaDruga));
